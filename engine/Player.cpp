edbca42c7c96e2fae38a49201759848f89d44466
dimock <dimock1973@gmail.com>
2012-02-16 21:14:30 +0400

    restore check extension conditions

--- engine/Player.cpp
+++ engine/Player.cpp
@@ -1402,7 +1402,7 @@ bool Player::do_extension(int depth, int ply, ScoreType alpha, ScoreType betta)
       return true;
 #else
     // if there is great alpha extend only dangerous checks, otherwise extend always
-    if ( alpha <= Figure::figureWeight_[Figure::TypeQueen] )
+    if ( alpha <= Figure::figureWeight_[Figure::TypeRook]+Figure::figureWeight_[Figure::TypeKnight] )
       return true;
 #endif
   }
