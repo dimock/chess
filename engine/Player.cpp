29b3fb43511bb6607af92db2a9c939f06e2a9d87
dimock <dimock1973@gmail.com>
2012-02-20 22:25:05 +0400

    perform all checks until depth == 0

--- engine/Player.cpp
+++ engine/Player.cpp
@@ -894,7 +894,7 @@ ScoreType Player::captures(int depth, int ply, ScoreType alpha, ScoreType betta,
 
         THROW_IF( !board_.validMove(check), "move validation failed" );
 
-        if ( !do_check(check) )
+        if ( depth < 1 && !do_check(check) )
           continue;
 
         capture(depth, ply, alpha, betta, check, counter);
